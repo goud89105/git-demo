a1a8cb77d4056fded937b40c09492e3c7208e242
goud89105 <97938978+goud89105@users.noreply.github.com>
2023-02-18 12:59:30 +0530

    developer2 changes

--- src/main/java/com/git_demo/git_demo/App.java
+++ src/main/java/com/git_demo/git_demo/App.java
@@ -19,6 +19,11 @@ public class App
 	{
 		System.out.println("do some changes by dev2");
 	}
+	
+	public void deletion()
+	{
+		System.out.println("do some changes by dev2");
+	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
