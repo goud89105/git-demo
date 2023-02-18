929f35181f6560c0549690abf962d08006191091
Simplilearn GitHub <siddam.bharat@simplilearn.net>
2023-02-18 11:29:52 +0530

    changes

--- src/main/java/com/git_demo/git_demo/App.java
+++ src/main/java/com/git_demo/git_demo/App.java
@@ -10,6 +10,9 @@ public class App
 	public void add(String data)
 	{
 		System.out.println("dummy method");
+		if(data.equals("ravindra")) {
+			System.out.println("do some changes");
+		}
 	}
     public static void main( String[] args )
     {
